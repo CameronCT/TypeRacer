9aed00b09740333f1060b7e617118f2e8dd79066
Cameron <gnikgg@hotmail.com>
2017-06-02 16:43:02 -0700

    Fixed args var, also converted ints to dates

--- discord.py/app.py
+++ discord.py/app.py
@@ -3,6 +3,10 @@ import sys
 import urllib.request
 import json
 import discord
+import datetime
+
+datetime.datetime.fromtimestamp(int("1284101485")
+    ).strftime('%Y-%m-%d %H:%M:%S')
 
 client = discord.Client() # pylint: disable=C0103
 
@@ -20,11 +24,13 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    args = message.content.split(' ', 1)
-    if args[1]:
+    """ When user sends a message """
+    # pylint: disable=C0301
+
+    args = message.content.split(' ')
+    if args:
         cmd = args[1]
 
-    """ When user sends a message """
     if message.content.startswith('!test'):
         counter = 0
         tmp = await client.send_message(message.channel, 'Calculating messages...')
@@ -34,33 +40,36 @@ async def on_message(message):
 
         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
 
-    if message.content.startswith('!stats') or message.content.startswith('!s'):
+    elif message.content.startswith('!stats') or message.content.startswith('!s'):
         if cmd is None:
             await client.send_message(message.channel, '!stats <username')
 
         response = urllib.request.urlopen('http://typeracerdata.com/api?username=' + cmd)
         data = data = json.load(response)
 
-        if data is None:
-            await client.send_message(message.channel, 'The username you have entered does not exist, please try again!') # pylint: disable=C0301
+        timezone = 'UTC'
+        marathon = datetime.datetime.fromtimestamp(float(data['account']['marathon_start'])).strftime('%Y-%m-%d %H:%M:%S') + ' ' + timezone
+        lastimport = datetime.datetime.fromtimestamp(float(data['account']['last_import'])).strftime('%Y-%m-%d %H:%M:%S') + ' ' + timezone
 
-        elif data:
+        if data:
             embed = discord.Embed(title='Statistics for ' + cmd, colour=0xDEADBF)
             embed.add_field(name='\u200b', value='__**General Statistics**__', inline=False)
-            embed.add_field(name='Races:', value=data['account']['races'] + ' (' + data['account']['wins'] + ' won)') # pylint: disable=C0301
+            embed.add_field(name='Races:', value=data['account']['races'] + ' (' + data['account']['wins'] + ' won)')
             embed.add_field(name='Texts:', value=data['account']['texts_raced'])
-            embed.add_field(name='Marathon:', value=data['account']['marathon_total'] + ' on ' + str(data['account']['marathon_start'])) # pylint: disable=C0301
+            embed.add_field(name='Marathon:', value=data['account']['marathon_total'] + ' on ' + marathon)
             embed.add_field(name='\u200b', value='__**WPM Statistics**__', inline=False)
-            embed.add_field(name='Career Avg:', value='%.2f' % float(data['account']['wpm_life']) + ' WPM') # pylint: disable=C0301
-            embed.add_field(name='Highest:', value='%.2f' % float(data['account']['wpm_highest']) + ' WPM') # pylint: disable=C0301
-            embed.add_field(name='Text Bests:', value='%.2f' % float(data['account']['wpm_textbests']) + ' WPM') # pylint: disable=C0301
-            embed.add_field(name='Last 10:', value='%.2f' % float(data['account']['wpm_last10']) + ' WPM ' + '(' + '%.2f' % float(data['account']['wpm_bestlast10']) + ' peak)') # pylint: disable=C0301
-            embed.set_footer(text='Last Imported: ' + str(data['account']['last_import']))
+            embed.add_field(name='Career Avg:', value='%.2f' % float(data['account']['wpm_life']) + ' WPM')
+            embed.add_field(name='Highest:', value='%.2f' % float(data['account']['wpm_highest']) + ' WPM')
+            embed.add_field(name='Text Bests:', value='%.2f' % float(data['account']['wpm_textbests']) + ' WPM')
+            embed.add_field(name='Last 10:', value='%.2f' % float(data['account']['wpm_last10']) + ' WPM ' + '(' + '%.2f' % float(data['account']['wpm_bestlast10']) + ' peak)')
+            embed.set_footer(text='Last Imported: ' + lastimport)
 
             await client.send_message(message.channel, embed=embed)
+        elif data is None or False:
+            await client.send_message(message.channel, 'The username you have entered does not exist, please try again!')
 
-    if message.content.startswith('!exit'):
-        await client.send_message(message.channel, 'Closing')
+    elif message.content.startswith('!exit'):
         sys.exit()
+        await client.send_message(message.channel, 'Closing')
 
 client.run(CONFIG['Discord'])
