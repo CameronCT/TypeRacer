4bb565db4507b503ac0aa1fcbb76c4b6a2e99c57
Cameron <gnikgg@hotmail.com>
2017-06-05 00:08:16 -0700

    Removed command restrictions

--- discord.py/cmds/report.py
+++ discord.py/cmds/report.py
@@ -12,20 +12,16 @@ from methods import send_reply
 
 async def execute(client, message, config):
     """ Executes the command !stats """
+    args = message.content.split(' ')
 
-    if config['Commands']['report'] is True or message.channel.permissions_for(message.author).kick_members or message.author.id == config['Owner']:
-        args = message.content.split(' ')
+    reportstamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') + ' UTC'
 
-        reportstamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') + ' UTC'
-
-        if len(args) <= 2:
-            await send_reply(client, message.channel, message.author.id, 'Please use the correct syntax. !report <username> <log>', True)
-        else:
-            embed = discord.Embed(colour=0xFF0000)
-            embed.add_field(name='User', value=args[1], inline=False)
-            embed.add_field(name='Message', value=args[2], inline=False)
-            embed.set_footer(text='Reported by ' + message.author.nick + ' on ' + reportstamp + '.')
-
-            await client.send_message(client.get_channel(config['Channels']['report']), embed=embed)
+    if len(args) <= 2:
+        await send_reply(client, message.channel, message.author.id, 'Please use the correct syntax. !report <username> <log>', True)
     else:
-        await send_reply(client, message.channel, message.author.id, 'this command has been temporarily disabled!')
+        embed = discord.Embed(colour=0xFF0000)
+        embed.add_field(name='User', value=args[1], inline=False)
+        embed.add_field(name='Message', value=args[2], inline=False)
+        embed.set_footer(text='Reported by ' + message.author.nick + ' on ' + reportstamp + '.')
+
+        await client.send_message(client.get_channel(config['Channels']['report']), embed=embed)
