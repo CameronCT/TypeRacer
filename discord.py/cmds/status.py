c80b9b7cfa96525dc618ce4e716e12c572cf8291
Cameron <gnikgg@hotmail.com>
2017-06-04 22:57:18 -0700

    Added check on !set to make it cannot be abused

--- discord.py/cmds/status.py
+++ discord.py/cmds/status.py
@@ -11,31 +11,35 @@ from methods import send_reply
 
 async def execute(client, message, config):
     """ Executes the command !set """
-    err = ''
-    args = message.content.split(' ')
+    if message.channel.permissions_for(message.author).kick_members or message.author.id == config['Owner']:
+        err = ''
+        args = message.content.split(' ')
 
-    if len(args) <= 2:
-        await send_reply(client, message.channel, message.author.id, 'Please use the correct syntax. **!set <help/stats/dev/etc> <on/off>**', True)
-    else:
-        if args[1] not in config['Commands']:
-            err = 'the command you have entered could not be found, please try again!'
-
-        if args[2].lower() not in ('on', 'off'):
-            err = 'you can only set the command to true or false, please try again!'
-
-        if not err:
-            cmd = True
-            if args[2].lower() == 'on':
-                cmd = True
-            else:
-                cmd = False
-
-            config['Commands'][args[1]] = cmd
-
-
-            await send_reply(client, message.channel, message.author.id, 'you have set **!' + args[1] + '** to **' + args[2] + '**!')
+        if len(args) <= 2:
+            await send_reply(client, message.channel, message.author.id, 'Please use the correct syntax. **!set <help/stats/dev/etc> <on/off>**', True)
         else:
-            await send_reply(client, message.channel, message.author.id, err)
+            if args[1] not in config['Commands']:
+                err = 'the command you have entered could not be found, please try again!'
+
+            if args[2].lower() not in ('on', 'off'):
+                err = 'you can only set the command to true or false, please try again!'
+
+            if not err:
+                cmd = True
+                if args[2].lower() == 'on':
+                    cmd = True
+                else:
+                    cmd = False
+
+                config['Commands'][args[1]] = cmd
+
+
+                await send_reply(client, message.channel, message.author.id, 'you have set **!' + args[1] + '** to **' + args[2] + '**!')
+            else:
+                await send_reply(client, message.channel, message.author.id, err)
+    else:
+        await send_reply(client, message.channel, message.author.id, 'you are not authorized to perform this command!')
+
 
 async def execute_status(client, message, config):
     """ Executes the command !set """
