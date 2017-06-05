72c246c417fe5e402d5a14aa3310da8c8f07ba78
Cameron <gnikgg@hotmail.com>
2017-06-04 23:23:19 -0700

    Added !report, have a few more changes planned

--- discord.py/cmds/report.py
+++ discord.py/cmds/report.py
@@ -21,13 +21,12 @@ async def execute(client, message, config):
         if len(args) <= 2:
             await send_reply(client, message.channel, message.author.id, 'Please use the correct syntax. !report <username> <log>', True)
         else:
-            embed = discord.Embed(colour=0x00e5ee, )
-            embed.set_author(name='Reported by ' + message.author.nick)
-            embed.set_thumbnail(url='https://d30y9cdsu7xlg0.cloudfront.net/png/1306-200.png')
+            embed = discord.Embed(colour=0xFF0000, )
+            embed.set_author(name='TypeRacer Bot')
             embed.add_field(name='User:', value=args[1], inline=False)
             embed.add_field(name='Message:', value=args[2], inline=False)
-            embed.set_footer(text='Last commit on ' + reportstamp + '.')
+            embed.set_footer(text='Reported by ' + message.author.nick + ' on ' + reportstamp + '.')
 
-            await client.send_message(config['Channels']['report'], embed=embed)
+            await client.send_message(client.get_channel(config['Channels']['report']), embed=embed)
     else:
         await send_reply(client, message.channel, message.author.id, 'this command has been temporarily disabled!')
