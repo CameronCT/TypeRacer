393191862246e077d9e51ffd0c952b7a171036df
Cameron <gnikgg@hotmail.com>
2017-06-07 01:15:17 -0700

    Added !pas command

---
+++ discord.py/cmds/pas.py
@@ -0,0 +1,18 @@
+"""
+    File: pas.py
+    Description: Executes the command !pissandshit
+    Last Modified: 6/4/2017
+
+    This command was a community demanded command, one of the
+    users like's saying "piss and shit" and is now a command.
+"""
+# pylint: disable=C0301
+
+import time
+import datetime
+import discord
+from methods import send_reply
+
+async def execute(client, message, config):
+    """ Executes the command !pissandshit """
+    await send_reply(client, message.channel, message.author.id, '🐸')
